3b41313152a48d85bfa3bb384a26b447c9908f83
walleri18 <walleri18@yandex.ru>
2015-07-25 17:48:54 +0300

    Листинг 1.6. Оператор присваивания

--- Васильев С++/Исходный код.cpp
+++ Васильев С++/Исходный код.cpp
@@ -6,53 +6,12 @@ int main(void)
 {
 	setlocale(LC_ALL, "rus");
 
-	int n, m, i = 3, j = 3;
+	int n, m;
 
-	cout << "At the beginning:" << endl;
-	cout << "i = " << i << endl;
-	cout << "j = " << j << endl;
-	cout << "After command n = i++ :" << endl;
-	
-	n = i++; // n = 3, i = 4
+	n = (m = 6) + 3;
 
-	cout << "n = " << n << endl;
-	cout << "i = " << i << endl;
-	cout << " After command m = ++j :" << endl;
-
-	m = ++j; // m = 4, n = 4
-
-	cout << "m = " << m << endl;
-	cout << "j = " << j << endl;
-	cout << " After command n = (--i) * (i--) :" << endl;
-
-	n = (--i) * (i--);// n = 9, i = 2
-
-	cout << "n = " << n << endl;
-	cout << "i = " << i << endl;
-	cout << " After command m = (--j) * (--j) :" << endl;
-
-	m = (--j) * (--j);// m = 4, j = 2
-
-	cout << "m = " << m << endl;
-	cout << "j = " << j << endl;
-	cout << " After command n = (--i) * (i++) :" << endl;
-
-	n = (--i) * (i++);// n = 1, i = 2
-
-	cout << "n = " << n << endl;
-	cout << "i = " << i << endl;
-	cout << " After command m = (j--) * (++j) :" << endl;
-
-	m = (j--) * (++j);// m = 9, j = 2
-
-	cout << "m = " << m << endl;
-	cout << "j = " << j << endl;
-	cout << " After command n = (--i) * (++i) :" << endl;
-
-	n = (--i) * (++i);// n = 4, i = 2
-
-	cout << "n = " << n << endl;
-	cout << "i = " << i << endl;
+	cout << m << endl;
+	cout << n << endl;
 
 	system("pause");
 	return 0;
