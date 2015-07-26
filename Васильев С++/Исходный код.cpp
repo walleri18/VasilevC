f46522a16952caebba8b99ff04ac1ee1af563dd4
walleri18 <walleri18@yandex.ru>
2015-07-26 21:05:12 +0300

    Листинг 1.11. Средняя скорость мотоциклиста

--- Васильев С++/Исходный код.cpp
+++ Васильев С++/Исходный код.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
@@ -7,37 +6,25 @@ int main(void)
 {
 	setlocale(LC_ALL, "rus");
 
-	// Константа - ускорения свободного падения и число pi:
-	const double g = 9.8;
-	const double pi = 3.1415;
-
-	// Начальные и расчётные параметры задачи
-	// (скорость, угол и время полёта):
-	double v, alpha, T;
-
-	// Момент времени и координаты:
-	double t, x, y;
+	// Параметры задачи:
+	double S1, S2, t1, t2, V;
 
 	// Ввод параметров:
-	cout << "Enter speed v = ";
-	cin >> v;
+	cout << "Enter S1 = ";
+	cin >> S1;
 
-	cout << "Enter angel alpha = ";
-	cin >> alpha;
+	cout << "Enter S2 = ";
+	cin >> S2;
 
-	alpha = alpha * pi / 180;
+	cout << "Enter t1 = ";
+	cin >> t1;
 
-	T = 2 * v * sin(alpha) / g;
+	cout << "Enter t2 = ";
+	cin >> t2;
+	
+	V = (S1 + S2) / (t1 + t2);
 
-	cout << "Enter time t < " << T << ": ";
-	cin >> t;
-
-	x = v * t * cos(alpha);
-
-	y = v * t * sin(alpha) - g * t * t / 2;
-
-	cout << "x = " << x << endl;
-	cout << "y = " << y << endl;
+	cout << "Speed V = " << V << endl;
 
 	system("pause");
 	return 0;
