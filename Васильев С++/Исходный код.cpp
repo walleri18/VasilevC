83e44a97d8c3793cc819023fd617a507d061ee8a
walleri18 <walleri18@yandex.ru>
2015-07-26 21:00:12 +0300

    Листинг 1.10. Тело брошено под углом к горизонту

--- Васильев С++/Исходный код.cpp
+++ Васильев С++/Исходный код.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -6,16 +7,37 @@ int main(void)
 {
 	setlocale(LC_ALL, "rus");
 
-	int n;
+	// Константа - ускорения свободного падения и число pi:
+	const double g = 9.8;
+	const double pi = 3.1415;
 
-	double x;
+	// Начальные и расчётные параметры задачи
+	// (скорость, угол и время полёта):
+	double v, alpha, T;
 
-	cout << "Enter n = ";
-	cin >> n;
-	
-	x = n > 0 ? 5.4 : 3.2;
+	// Момент времени и координаты:
+	double t, x, y;
+
+	// Ввод параметров:
+	cout << "Enter speed v = ";
+	cin >> v;
+
+	cout << "Enter angel alpha = ";
+	cin >> alpha;
+
+	alpha = alpha * pi / 180;
+
+	T = 2 * v * sin(alpha) / g;
+
+	cout << "Enter time t < " << T << ": ";
+	cin >> t;
+
+	x = v * t * cos(alpha);
+
+	y = v * t * sin(alpha) - g * t * t / 2;
 
 	cout << "x = " << x << endl;
+	cout << "y = " << y << endl;
 
 	system("pause");
 	return 0;
