99ecce845c2e4f1bb95eb4e082b6b0a07f5c787a
walleri18 <walleri18@yandex.ru>
2015-07-31 22:29:57 +0300

    Задачи для самостоятельного решения. Глава 1. Задача 3.

--- Васильев С++/Исходный код.cpp
+++ Васильев С++/Исходный код.cpp
@@ -10,25 +10,33 @@ int main(void)
 	//  онстанта силы гравитации:
 	const double g = 9.8;
 
-	// ќбъ€вление и запись значени€ начальной скорости:
-	double V;
+	// ќбъ€вление и запись максимальной высоты подъЄма:
+	double H;
 
-	cout << "Enter V = ";
-	cin >> V;
+	cout << "Enter H = ";
+	cin >> H;
 
-	// ќбъ€вление и запись значени€ угла броска:
-	double alpha;
-
-	cout << "Enter alpha = ";
-	cin >> alpha;
-
-	// ќбъ€вление и расчЄт дальности полЄта тела:
+	// ќбъ€вление и запись дальности полЄта:
 	double L;
 
-	L = (V * V * sin(2 * alpha)) / g;
+	cout << "Enter L = ";
+	cin >> L;
 
-	// ¬ывод значени€ дальности полЄта:
-	cout << "L = " << L << endl;
+	// ќбъ€вление и расчЄт угла:
+	double alpha;
+
+	alpha = atan(4 * H / L);
+
+	// ќбъ€вление и расчЄт начальной скорости:
+	double V;
+
+	V = sqrt((g * L) / sin(2 * alpha));
+
+	// ¬ывод угла и начальной скорости:
+	cout << "Alpha = " << alpha << endl;
+
+	cout << "V = " << V << endl
+		;
 
 	system("pause");
 	return 0;
