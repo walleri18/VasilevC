267a80b63a7fcbfaa6dc2fa83e742d6c7eea32e7
walleri18 <walleri18@yandex.ru>
2015-08-01 18:22:43 +0300

    Задачи для самостоятельного решения. Глава 1. Задача 5.

--- Васильев С++/Исходный код.cpp
+++ Васильев С++/Исходный код.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
@@ -9,55 +8,32 @@ int main(void)
 
 	//  онстанта силы гравитации:
 	const double g = 9.8;
-
-	//  онстанта ѕ»:
-	const double PI = 3.14;
-
-	// ќбъ€вление и ввод высоты обрыва:
+	
+	// ќбъ€вление и ввод высоты полЄта самолЄта:
 	double H;
 
 	cout << "Enter H = ";
 	cin >> H;
 
-	// ќбъ€вление и ввод начальной скорости:
+	// ќбъ€вление и ввод скорости самолЄта:
 	double V;
 
 	cout << "Enter V = ";
 	cin >> V;
-
-	// ќбъ€вление и ввод угла броска в градусах:
-	double alpha;
-
-	cout << "Enter alpha = ";
-	cin >> alpha;
-
-	// ѕеревод значени€ угла из градусов в радианы:
-	alpha = alpha * PI / 180;
-
-	// ќбъ€вление и расчЄт вемени полЄта камн€:
+	
+	// ќбъ€вление и расчЄт времени подлЄтного рассто€ни€:
 	double T;
 
-	T = (V * sin(alpha) / g) * (1 * sqrt(1 + (2 * g * H) / (V * V * sin(alpha) * sin(alpha))));
+	T = sqrt(2 * H / g);
 
-	// ќбъ€вление и ввод времени дл€ определени€ положени€ камн€:
-	double t;
+	// ќбъ€вление и расчЄт рассто€ни€ подлЄта к наземному объекту:
+	double S;
 
-	cout << "Enter t < " << T << " = ";
-	cin >> t;
-
-	// ќбъ€вление и вычисление координаты x:
-	double x;
-	
-	x = (t > T) ? (V * T * cos(alpha)) : (V * t * cos(alpha));
-
-	// ќбъ€вление и вычисление координаты y:
-	double y;
-
-	y = (t > T) ? (H + V * T * sin(alpha) - g * T * T / 2) : (H + V * t * sin(alpha) - g * t * t / 2);
+	S = V * T;
 
 	// ¬ывод результатов:
-	cout << "X(t) = " << x << endl;
-	cout << "Y(t) = " << y << endl;
+	cout << "T = " << T << endl; /* ¬рем€ подлЄта */
+	cout << "S = " << S << endl; /* –ассто€ние подлЄта */
 
 	system("pause");
 	return 0;
