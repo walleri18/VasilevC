d848c1106d87a3ea282874a3b4a7037945ff53a3
walleri18 <walleri18@yandex.ru>
2015-08-01 18:10:47 +0300

    Задачи для самостоятельного решения. Глава 1. Задача 4.

--- Васильев С++/Исходный код.cpp
+++ Васильев С++/Исходный код.cpp
@@ -10,33 +10,54 @@ int main(void)
 	//  онстанта силы гравитации:
 	const double g = 9.8;
 
-	// ќбъ€вление и запись максимальной высоты подъЄма:
+	//  онстанта ѕ»:
+	const double PI = 3.14;
+
+	// ќбъ€вление и ввод высоты обрыва:
 	double H;
 
 	cout << "Enter H = ";
 	cin >> H;
 
-	// ќбъ€вление и запись дальности полЄта:
-	double L;
-
-	cout << "Enter L = ";
-	cin >> L;
-
-	// ќбъ€вление и расчЄт угла:
-	double alpha;
-
-	alpha = atan(4 * H / L);
-
-	// ќбъ€вление и расчЄт начальной скорости:
+	// ќбъ€вление и ввод начальной скорости:
 	double V;
 
-	V = sqrt((g * L) / sin(2 * alpha));
+	cout << "Enter V = ";
+	cin >> V;
 
-	// ¬ывод угла и начальной скорости:
-	cout << "Alpha = " << alpha << endl;
+	// ќбъ€вление и ввод угла броска в градусах:
+	double alpha;
 
-	cout << "V = " << V << endl
-		;
+	cout << "Enter alpha = ";
+	cin >> alpha;
+
+	// ѕеревод значени€ угла из градусов в радианы:
+	alpha = alpha * PI / 180;
+
+	// ќбъ€вление и расчЄт вемени полЄта камн€:
+	double T;
+
+	T = (V * sin(alpha) / g) * (1 * sqrt(1 + (2 * g * H) / (V * V * sin(alpha) * sin(alpha))));
+
+	// ќбъ€вление и ввод времени дл€ определени€ положени€ камн€:
+	double t;
+
+	cout << "Enter t < " << T << " = ";
+	cin >> t;
+
+	// ќбъ€вление и вычисление координаты x:
+	double x;
+	
+	x = (t > T) ? (V * T * cos(alpha)) : (V * t * cos(alpha));
+
+	// ќбъ€вление и вычисление координаты y:
+	double y;
+
+	y = (t > T) ? (H + V * T * sin(alpha) - g * T * T / 2) : (H + V * t * sin(alpha) - g * t * t / 2);
+
+	// ¬ывод результатов:
+	cout << "X(t) = " << x << endl;
+	cout << "Y(t) = " << y << endl;
 
 	system("pause");
 	return 0;
