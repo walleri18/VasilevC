4419f6adecca6e19080a3bce6864ccf505b61fa2
walleri18 <walleri18@yandex.ru>
2015-08-02 10:44:37 +0300

    Задачи для самостоятельного решения. Глава 1. Задача 9.

--- Васильев С++/Исходный код.cpp
+++ Васильев С++/Исходный код.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+#include <cmath>
 
 using namespace std;
 
@@ -7,51 +7,33 @@ int main(void)
 {
 	setlocale(LC_ALL, "rus");
 
-	// ќбъ€вление и запись времени движени€ проезда:
-	double t;
+	// ќбъ€вление константы PI:
+	const double PI = 3.14;
 
-	cout << "Enter t = ";
-	cin >> t;
+	// ќбъ€вление и запись частоты колебани€ ма€тника:
+	double omega;
 
-	// ќбъ€вление и запись скорости движени€ поезда:
-	double V;
+	cout << "Enter omega = ";
+	cin >> omega;
 
-	cout << "Enter V = ";
-	cin >> V;
+	// ќбъ€вление и запись некого k:
+	double k;
 
-	// ќбъ€вление и вычисление ускорение при равнозамедленном движении:
-	double a;
+	cout << "Enter k = ";
+	cin >> k;
 
-	a = V / t;
+	// ќбъ€вление и вычисление нулевой частоты колебани€ ма€тника:
+	double phi0;
 
-	// ќбъ€вление и вычисление первого пути движени€:
-	double S1;
+	phi0 = asin(1 / k);
 
-	S1 = V * t;
+	// ќбъ€вление и вычисление момента отклонени€ ма€тника на максимальное рассто€ние:
+	double T;
 
-	// ќбъ€вление и вычисление второго пути движени€:
-	double S2;
+	T = (PI / 2 - phi0) / omega;
 
-	S2 = a * t * t / 2;
-
-	// ќбъ€вление и вычисление общего пути, пройденного поездом:
-	double S;
-
-	S = S1 + S2;
-
-	// ќбъ€вление и вычисление проверочной переменной пути полного движени€ поезда:
-	double SP;
-
-	SP = 3 * V * t / 2;
-
-	// —равнение S & SP:
-	string messageProverka = "";
-	(S == SP) ? (messageProverka = "—оотношени€ равны!") : (messageProverka = "—оотношени€ не равны!");
-	
-	// ¬ывод результатов:
-	cout << "S = " << S << endl;
-	cout << "SP = " << SP << endl;
-	cout << messageProverka << endl;
+	// ¬ывод результата:
+	cout << "T = " << T << endl;
 
 	system("pause");
 	return 0;
